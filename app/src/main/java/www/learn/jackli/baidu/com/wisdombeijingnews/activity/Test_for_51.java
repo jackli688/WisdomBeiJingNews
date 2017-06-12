de2d7ebca43643633619f037928beccbccd042e9
jackli688 <lijiwei218@163.com>
2017-06-12 23:24:47 +0800

    1.新建一个Test_for_51的类，作为测试。

---
+++ app/src/main/java/www/learn/jackli/baidu/com/wisdombeijingnews/activity/Test_for_51.java
@@ -0,0 +1,10 @@
+package www.learn.jackli.baidu.com.wisdombeijingnews.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jackli on 2017/6/12.
+ */
+
+public class Test_for_51 extends AppCompatActivity {
+}
