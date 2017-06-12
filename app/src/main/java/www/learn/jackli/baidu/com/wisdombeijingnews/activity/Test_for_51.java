e912e057d01a39f7204696896a9e11e445d6ec54
jackli688 <lijiwei218@163.com>
2017-06-12 23:26:12 +0800

    1.Test_for_51中添加onCreate方法。

--- app/src/main/java/www/learn/jackli/baidu/com/wisdombeijingnews/activity/Test_for_51.java
+++ app/src/main/java/www/learn/jackli/baidu/com/wisdombeijingnews/activity/Test_for_51.java
@@ -1,5 +1,7 @@
 package www.learn.jackli.baidu.com.wisdombeijingnews.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +9,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class Test_for_51 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
